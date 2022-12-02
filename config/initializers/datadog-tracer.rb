2ed8c8bd02fb4db08005a8adba52af73dcfbdffa
Steven <admin@dark-it.net>
2022-12-02 09:00:38 +0000

    Re-Fix custom changes

---
+++ config/initializers/datadog-tracer.rb
@@ -0,0 +1,10 @@
+Datadog.configure do |c|
+      c.service = 'layer8-mastodon'
+      c.tracing.instrument :rails
+      c.tracing.instrument :sidekiq
+      c.tracing.instrument :active_record, service_name: 'layer8-mastodon-active-record'
+      c.tracing.instrument :redis, service_name: 'layer8-mastodon-redis'
+      c.tracing.instrument :pg, service_name: 'layer8-mastodon-postgres'
+      c.tracing.instrument :elasticsearch, service_name: 'layer8-mastodon-elasticsearch'
+      c.tracing.instrument :httprb, service_name: 'layer8-mastodon-httprb'
+end
